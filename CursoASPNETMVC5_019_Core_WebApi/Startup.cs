81f7df20d3e895b52831b76750ec38f676ed0354
jmampelf <87749510+jmampelf@users.noreply.github.com>
2024-06-08 00:40:26 +0200

    primer cambio despues de crear repositorio en cloud

--- CursoASPNETMVC5_019_Core_WebApi/Startup.cs
+++ CursoASPNETMVC5_019_Core_WebApi/Startup.cs
@@ -21,7 +21,7 @@ namespace CursoASPNETMVC5_019_Core_WebApi
 
         public IConfiguration Configuration { get; }
 
-        // .This method gets called by the runtime. Use this method to add services to the container.
+        // ..This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
